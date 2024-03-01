c8d63646d505af694599a072ca72d744f08b69c7
BEduease <132479089+BEduease@users.noreply.github.com>
2024-03-01 16:34:50 +0000

    changed token, added student model

--- static/js/streams.js
+++ static/js/streams.js
@@ -1,6 +1,6 @@
 const APP_ID = 'cb3fa3b62ac94450847c7b1c7d2e35b1'
 const  CHANNEL = 'main'
-const  TOKEN = '007eJxTYLAO6jK/5ZroeWhn2oEFJVliM6pb79b+3jZ10d+Fontntl5TYEhOMk5LNE4yM0pMtjQxMTWwMDFPNk8yTDZPMUo1Nk0yTAp7kNoQyMigFD6ThZEBAkF8FobcxMw8BgYA0+8hFQ=='
+const  TOKEN = '007eJxTYEj1LffgFftzIs5sw9zui/PN+L6uKdnJqsdnoZtjujp7xw0FhuQk47RE4yQzo8RkSxMTUwMLE/Nk8yTDZPMUo1Rj0yRD/9cPUxsCGRk2Gq9kYWSAQBCfhSE3MTOPgQEA8NIevA=='
 let UID;
 
 const client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'})
